0ebe66cd942e91fc70d055c6c05820121028f858
Bellamkonda Sai Mukesh Ram <mukeshrambellamkonda@gmail.com>
2025-12-09 14:06:11 +0000

    connecting to backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
 const handleResponse = async (res) => {
   const data = await res.json().catch(() => ({}));
